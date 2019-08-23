4005fc247f483c338a46286f9a7508e161f07f36
Evan Bailey <baileyevang@gmail.com>
2019-08-23 15:38:13 -0700

    Get HP from StatBlock HTML

--- src/scripts/contentscript.js
+++ src/scripts/contentscript.js
@@ -4,11 +4,22 @@ function getNameFrom(element) {
   return element.querySelector(".mon-stat-block__name a").innerHTML.trim();
 }
 
+function getHpFrom(element) {
+  const label = Array.from(element.querySelectorAll(".mon-stat-block__attribute-label")).filter(e => e.innerHTML == "Hit Points")[0];
+  const value = parseInt(label.parentElement.querySelector(".mon-stat-block__attribute-data-value").innerHTML.trim());
+  const notes = label.parentElement.querySelector(".mon-stat-block__attribute-data-extra").innerHTML.trim();
+  return {
+    Value: value,
+    Notes: notes
+  };
+}
+
 var extractStatBlock = () => {
   const statBlockElement = document.querySelector(".mon-stat-block");
 
   const statBlock = {
-    Name: getNameFrom(statBlockElement)
+    Name: getNameFrom(statBlockElement),
+    HP: getHpFrom(statBlockElement)
   };
 
   return statBlock;
