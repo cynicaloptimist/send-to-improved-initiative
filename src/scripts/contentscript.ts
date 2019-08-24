0241222f6b145d94155509ad39c2cf9c725ded01
Evan Bailey <baileyevang@gmail.com>
2019-08-24 10:45:09 -0700

    Extract method to file

--- src/scripts/contentscript.ts
+++ src/scripts/contentscript.ts
@@ -1,32 +1,5 @@
 import ext from "./utils/ext";
-import cash, { Cash } from "cash-dom";
-
-function getNameFrom(element: Cash) {
-  return element.find(".mon-stat-block__name a").text().trim();
-}
-
-function getHpFrom(element: Cash) {
-  const label = element.find(".mon-stat-block__attribute-label")
-    .filter((_, e: Element) => e.innerHTML.trim() == "Hit Points" ).first();
-  const value = parseInt(label.parent().find(".mon-stat-block__attribute-data-value").text().trim());
-  const notes = label.parent().find(".mon-stat-block__attribute-data-extra").text().trim();
-  return {
-    Value: value,
-    Notes: notes
-  };
-}
-
-var extractStatBlock = () => {
-  const doc = cash(document);
-  const statBlockElement = doc.find(".mon-stat-block");
-
-  const statBlock = {
-    Name: getNameFrom(statBlockElement),
-    HP: getHpFrom(statBlockElement)
-  };
-
-  return statBlock;
-}
+import { extractStatBlock } from "./extractstatblock";
 
 function onRequest(request, sender, sendResponse) {
   if (request.action === 'process-page') {
---
+++ src/scripts/extractstatblock.ts
@@ -0,0 +1,28 @@
+import cash, { Cash } from "cash-dom";
+
+function getNameFrom(element: Cash) {
+  return element.find(".mon-stat-block__name a").text().trim();
+}
+
+function getHpFrom(element: Cash) {
+  const label = element.find(".mon-stat-block__attribute-label")
+    .filter((_, e: Element) => e.innerHTML.trim() == "Hit Points" ).first();
+  const value = parseInt(label.parent().find(".mon-stat-block__attribute-data-value").text().trim());
+  const notes = label.parent().find(".mon-stat-block__attribute-data-extra").text().trim();
+  return {
+    Value: value,
+    Notes: notes
+  };
+}
+
+export const extractStatBlock = () => {
+  const doc = cash(document);
+  const statBlockElement = doc.find(".mon-stat-block");
+
+  const statBlock = {
+    Name: getNameFrom(statBlockElement),
+    HP: getHpFrom(statBlockElement)
+  };
+
+  return statBlock;
+}
