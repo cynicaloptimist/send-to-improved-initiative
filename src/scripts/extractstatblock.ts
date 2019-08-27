68c8736275025556ce5a05ae06c5546ce87cafca
Evan Bailey <baileyevang@gmail.com>
2019-08-26 23:25:47 -0700

    get various comma-separated "tidbits"

--- src/scripts/extractstatblock.ts
+++ src/scripts/extractstatblock.ts
@@ -6,24 +6,56 @@ export const extractStatBlock = () => {
   const statBlockElement = doc.find(".mon-stat-block");
 
   const statBlock: Partial<StatBlock> = {
-    Source: doc.find(".monster-source").text().replace(/\s+/g," ").replace(" ,", ",").trim(),
+    Source: doc
+      .find(".monster-source")
+      .text()
+      .replace(/\s+/g, " ")
+      .replace(" ,", ",")
+      .trim(),
     Name: getName(statBlockElement),
     Type: getType(statBlockElement),
     HP: getHitPoints(statBlockElement),
     AC: getArmorClass(statBlockElement),
-    Speed: getSpeeds(statBlockElement),
     Abilities: getAbilities(statBlockElement),
+    Speed: getCommaSeparatedStrings(
+      statBlockElement,
+      "Speed"
+    ),
     // InitiativeModifier?: number,
     // InitiativeSpecialRoll?: "advantage" | "disadvantage" | "take-ten",
     // InitiativeAdvantage?: boolean,
-    // DamageVulnerabilities: string[],
-    // DamageResistances: string[],
-    // DamageImmunities: string[],
-    // ConditionImmunities: string[],
-    // Saves: NameAndModifier[],
-    // Skills: NameAndModifier[],
-    // Senses: string[],
-    // Languages: string[],
+    DamageVulnerabilities: getCommaSeparatedStrings(
+      statBlockElement,
+      "Damage Vulnerabilities"
+    ),
+    DamageResistances: getCommaSeparatedStrings(
+      statBlockElement,
+      "Damage Resistances"
+    ),
+    DamageImmunities: getCommaSeparatedStrings(
+      statBlockElement,
+      "Damage Immunities"
+    ),
+    ConditionImmunities: getCommaSeparatedStrings(
+      statBlockElement,
+      "Condition Immunities"
+    ),
+    Saves: getCommaSeparatedModifiers(
+      statBlockElement,
+      "Saving Throws"
+    ),
+    Skills: getCommaSeparatedModifiers(
+      statBlockElement,
+      "Saving Throws"
+    ),
+    Senses: getCommaSeparatedStrings(
+      statBlockElement,
+      "Senses"
+    ),
+    Languages: getCommaSeparatedStrings(
+      statBlockElement,
+      "Languages"
+    ),
     // Challenge: string,
     // Traits: NameAndContent[],
     // Actions: NameAndContent[],
@@ -35,14 +67,20 @@ export const extractStatBlock = () => {
   };
 
   return statBlock;
-}
+};
 
 function getName(element: Cash) {
-  return element.find(".mon-stat-block__name a").text().trim();
+  return element
+    .find(".mon-stat-block__name a")
+    .text()
+    .trim();
 }
 
 function getType(element: Cash) {
-  return element.find(".mon-stat-block__meta").text().trim();
+  return element
+    .find(".mon-stat-block__meta")
+    .text()
+    .trim();
 }
 
 function getArmorClass(element: Cash) {
@@ -54,22 +92,27 @@ function getHitPoints(element: Cash) {
 }
 
 function getAttribute(element: Cash, attributeName: string) {
-  const label = element.find(".mon-stat-block__attribute-label")
-    .filter((_, e: Element) => e.innerHTML.trim() == attributeName).first();
-  
-  const value = parseInt(label.parent().find(".mon-stat-block__attribute-data-value").text().trim());
-  const notes = label.parent().find(".mon-stat-block__attribute-data-extra").text().trim();
+  const label = element
+    .find(".mon-stat-block__attribute-label")
+    .filter((_, e: Element) => e.innerHTML.trim() == attributeName)
+    .first();
+
+  const value = parseInt(
+    label
+      .parent()
+      .find(".mon-stat-block__attribute-data-value")
+      .text()
+      .trim()
+  );
+  const notes = label
+    .parent()
+    .find(".mon-stat-block__attribute-data-extra")
+    .text()
+    .trim();
   return {
     Value: value,
     Notes: notes
   };
-}
-
-function getSpeeds(element: Cash) {
-  const label = element.find(".mon-stat-block__attribute-label")
-    .filter((_, e: Element) => e.innerHTML.trim() == "Speed").first();
-  const commaSeparatedSpeeds = label.parent().find(".mon-stat-block__attribute-data-value").text().trim();
-  return commaSeparatedSpeeds.split(",").map(s => s.trim());
 }
 
 function getAbilities(element: Cash): AbilityScores {
@@ -79,16 +122,45 @@ function getAbilities(element: Cash): AbilityScores {
     Con: getAbility(element, "con"),
     Int: getAbility(element, "int"),
     Wis: getAbility(element, "wis"),
-    Cha: getAbility(element, "cha"),
-  }
+    Cha: getAbility(element, "cha")
+  };
 }
 
 function getAbility(element: Cash, ability: string) {
   let score = 10;
-  const scoreText = element.find(`.ability-block__stat--${ability} .ability-block__score`).text();
+  const scoreText = element
+    .find(`.ability-block__stat--${ability} .ability-block__score`)
+    .text();
   try {
     score = parseInt(scoreText);
-  }
-  catch (e){}
+  } catch (e) {}
   return score;
 }
+
+function getCommaSeparatedStrings(element: Cash, tidbitName: string) {
+  const label = element.find(".mon-stat-block__tidbit-label")
+    .filter((_, e: Element) => e.innerHTML.trim() == tidbitName).first();
+
+  const commaDelimitedString = label.parent().find(".mon-stat-block__tidbit-data").text().trim();
+  if (commaDelimitedString.length > 0) {
+    return commaDelimitedString.split(/, ?/).map(s => s.trim());
+  }
+  return [];
+}
+
+function getCommaSeparatedModifiers(element: Cash, tidbitName: string) {
+  const entries = getCommaSeparatedStrings(element, tidbitName);
+  return entries.map(e => {
+    // Extract the last piece of the name/modifier, and parse an int from only that, ensuring the name can contain any manner of spacing.
+    const nameAndModifier = e.split(" ");
+    const modifierValue = parseInt(nameAndModifier.pop());
+
+    // Join the remaining string name, and trim outside spacing just in case.
+    return {
+      Name: nameAndModifier.join(" ").trim(),
+      Modifier: modifierValue
+    };
+  });
+}
+
+function getPowers(element: Cash, type: string) {}
