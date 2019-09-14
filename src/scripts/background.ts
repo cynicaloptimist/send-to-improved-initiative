6e5c1c4a7c1d089e51afa4888c0e177a9a6ac040
Evan Bailey <baileyevang@gmail.com>
2019-09-13 17:23:48 -0700

    Set target URL in options

--- src/scripts/background.ts
+++ src/scripts/background.ts
@@ -1,17 +1,27 @@
 import ext from "./utils/ext";
+import storage from "./utils/storage";
 const codec = require("json-url")("lzma");
 
 const runtime: typeof chrome.runtime = ext.runtime;
 const tabs: typeof chrome.tabs = ext.tabs;
 
+storage.get("target-url", values => {
+  if (values["target-url"] == null) {
+    storage.set({ "target-url": "https://www.improved-initiative.com/e/" })
+  }
+})
+
 runtime.onMessage.addListener(
-  async function(request, sender, sendResponse) {
+  function(request, sender, sendResponse) {
     if (request.action === "perform-save") {
-      const compressed = await codec.compress(JSON.stringify(request.importedStatBlock));
-      tabs.create({
-        url: "http://localhost/e/?s=" + encodeURIComponent(compressed)
+      storage.get("target-url", async values => {
+        const compressed = await codec.compress(JSON.stringify(request.importedStatBlock));
+        tabs.create({
+          url: values["target-url"] + "?s=" + encodeURIComponent(compressed)
+        });
+        sendResponse({ action: "saved" });
       });
-      sendResponse({ action: "saved" });
+      
     }
   }
 );
--- src/scripts/options.ts
+++ src/scripts/options.ts
@@ -1,29 +1,17 @@
 import storage from "./utils/storage";
 
-var colorSelectors = document.querySelectorAll(".js-radio");
+var targetUrlInput = document.querySelectorAll("input.target-url");
 
-var setColor = (color) => {
-  document.body.style.backgroundColor = color;
-};
-
-storage.get('color', function(resp) {
-  var color = resp.color;
-  var option;
-  if(color) {
-    option = document.querySelector(`.js-radio.${color}`);
-    setColor(color);
-  } else {
-    option = colorSelectors[0]
-  }
-
-  option.setAttribute("checked", "checked");
-});
-
-colorSelectors.forEach(function(el) {
-  el.addEventListener("click", function(e) {
-    var value = this.value;
-    storage.set({ color: value }, function() {
-      setColor(value);
+storage.get("target-url", values => {
+  targetUrlInput.forEach((i: HTMLInputElement) => {
+    i.value = values["target-url"];
+    i.addEventListener("change", e => {
+      const input = e.target as HTMLInputElement;
+      const newTargetUrl = input.value;
+      storage.set({
+        "target-url": newTargetUrl
+      });
     });
-  })
+  });
 })
+
