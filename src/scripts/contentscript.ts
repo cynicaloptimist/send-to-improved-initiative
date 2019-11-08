16c421e00ef5578b7cd790fe682f3ee7fa33a106
Evan Bailey <baileyevang@gmail.com>
2019-11-07 23:11:47 -0800

    annotate onRequest

--- src/scripts/contentscript.ts
+++ src/scripts/contentscript.ts
@@ -2,10 +2,12 @@ import ext from "./utils/ext";
 import { extractStatBlock } from "./extractstatblock";
 import { ScrapeStatBlockAction } from "./actions";
 
-function onRequest(request, sender, sendResponse) {
+function onRequest(request: any, sender, sendResponse: (response: any) => void) {
   if (request.action == ScrapeStatBlockAction) {
     sendResponse(extractStatBlock())
   }
 }
 
-ext.runtime.onMessage.addListener(onRequest);
+const runtime: typeof chrome.runtime = ext.runtime;
+
+runtime.onMessage.addListener(onRequest);
