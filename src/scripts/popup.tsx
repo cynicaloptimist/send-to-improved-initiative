4a841cf7ad23b7c18ff7308a1449d35ddb39740e
Evan Bailey <baileyevang@gmail.com>
2019-11-08 21:45:36 -0800

    Render popup with preact

--- src/scripts/popup.tsx
+++ src/scripts/popup.tsx
@@ -1,72 +1,114 @@
 import ext from "./utils/ext";
 import { StatBlock } from "./StatBlock";
 import { ScrapeStatBlockAction } from "./actions";
+import { h, render } from "preact";
 
 const tabs: typeof chrome.tabs = ext.tabs;
 const runtime: typeof chrome.runtime = ext.runtime;
 
-let importedStatBlock = {};
+const popup = document.getElementById("app");
 
-var popup = document.getElementById("app");
+render(<Importer />, popup);
 
-tabs.query({active: true, currentWindow: true}, function(tabs) {
+tabs.query({ active: true, currentWindow: true }, function(tabs) {
   var activeTab = tabs[0];
-  chrome.tabs.sendMessage(activeTab.id, { action: ScrapeStatBlockAction }, handleScrapedStatBlock);
+  chrome.tabs.sendMessage(
+    activeTab.id,
+    { action: ScrapeStatBlockAction },
+    handleScrapedStatBlock
+  );
 });
 
-var template = (data: StatBlock) => {
-  return (`
-  <div class="statblock-preview">
-    <h3>${data.Name}</h3>
-    <pre class="statblock-preview__json">${JSON.stringify(data, null, 1)}</pre>
-  </div>
-  ${verifyOrWarnPortrait(data)}
-  <div class="action-container">
-    <button id="save-btn" class="btn btn-primary">Import</button>
-  </div>
-  `);
+function handleScrapedStatBlock(data: StatBlock) {
+  if (data) {
+    render(<Importer importedStatBlock={data} />, popup);
+  } else {
+    render(<HelpText />, popup);
+  }
 }
 
-var verifyOrWarnPortrait = (data: StatBlock) => {
+function HelpText() {
+  return (
+    <div class="popup-content">
+      <p class="message">
+        Could not scrape a StatBlock from this page. Please ensure that you are
+        on a StatBlock <strong>Details</strong> page.
+      </p>
+      <a class="" href="https://www.dndbeyond.com/monsters">
+        D&amp;D Beyond Monsters
+      </a>
+    </div>
+  );
+}
+
+function Importer(props: { importedStatBlock?: StatBlock }) {
+  return (
+    <div class="popup-content">
+      <h1 class="app-name">Import to Improved Initiative</h1>
+      {props.importedStatBlock && (
+        <div id="display-container">
+          <div class="statblock-preview">
+            <h3>{props.importedStatBlock.Name}</h3>
+            <pre class="statblock-preview__json">
+              {JSON.stringify(props.importedStatBlock, null, 1)}
+            </pre>
+          </div>
+          {renderPortraitWarningIfNeeded(props.importedStatBlock)}
+          <div class="action-container">
+            <button
+              id="save-btn"
+              class="btn btn-primary"
+              onClick={importStatBlock(props.importedStatBlock)}
+            >
+              Import
+            </button>
+          </div>
+        </div>
+      )}
+      <footer>
+        <p>
+          <small>
+            <a href="#" class="js-options" onClick={openOptions}>
+              Options
+            </a>
+          </small>
+        </p>
+      </footer>
+    </div>
+  );
+}
+
+var renderPortraitWarningIfNeeded = (data: StatBlock) => {
   if (data.ImageURL.length > 0) {
     return "";
   }
-  return `<span class="warning">No portrait was found. Visit a Monster Details page to get the whole picture.</span>`;
-}
+  return (
+    <span class="warning">
+      No portrait was found. Visit a Monster Details page to get the whole
+      picture.
+    </span>
+  );
+};
 
-var renderMessage = (message) => {
-  var displayContainer = document.getElementById("display-container");
-  displayContainer.innerHTML = `<p class='message'>${message}</p>`;
-}
-
-var handleScrapedStatBlock = (data: StatBlock) => {
-  var displayContainer = document.getElementById("display-container")
-  if(data) {
-    var tmpl = template(data);
-    displayContainer.innerHTML = tmpl;  
-    importedStatBlock = data;
-  } else {
-    renderMessage("Could not scrape a StatBlock from this page. Please ensure that you are on a StatBlock <strong>Details</strong> page. " +
-                  `<a href="https://www.dndbeyond.com/monsters">D&D Beyond Monsters</a>`)
-  }
-}
-
-popup.addEventListener("click", function (e) {
-  const target: Element = e.target as Element;
-  if(target && target.matches("#save-btn")) {
-    e.preventDefault();
-    runtime.sendMessage({ action: "perform-save", importedStatBlock: importedStatBlock }, function(response) {
-      if(response && response.action === "saved") {
-        renderMessage("Your StatBlock was imported successfully!");
-      } else {
-        renderMessage("Sorry, there was an error while importing this StatBlock.");
+function importStatBlock(statBlock: StatBlock) {
+  return () =>
+    runtime.sendMessage(
+      { action: "perform-save", importedStatBlock: statBlock },
+      function(response) {
+        if (response && response.action === "saved") {
+          console.log("ok");
+        } else {
+          throw "Could not import statblock";
+        }
       }
-    })
-  }
-});
+    );
+}
+
+function openOptions() {
+  tabs.create({ url: ext.extension.getURL("options.html") });
+}
 
 var optionsLink = document.querySelector(".js-options");
 optionsLink.addEventListener("click", function(e) {
   e.preventDefault();
-  tabs.create({'url': ext.extension.getURL('options.html')});
-})
+});
