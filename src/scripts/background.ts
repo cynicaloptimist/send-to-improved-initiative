fd8e1eaff129a49687545460bc10b30b44d8c1e8
Evan Bailey <baileyevang@gmail.com>
2020-02-07 21:16:45 -0800

    Use options in background.ts

--- src/scripts/background.ts
+++ src/scripts/background.ts
@@ -1,27 +1,29 @@
 import ext from "./utils/ext";
 import storage from "./utils/storage";
+import { Options } from "./options";
 const codec = require("json-url")("lzma");
 
 const runtime: typeof chrome.runtime = ext.runtime;
 const tabs: typeof chrome.tabs = ext.tabs;
 
-storage.get("target-url", values => {
-  if (values["target-url"] == null) {
-    storage.set({ "target-url": "https://www.improved-initiative.com/e/" })
+storage.get(Object.values(Options), values => {
+  if (values[Options.TargetUrl] == null) {
+    storage.set({
+      [Options.TargetUrl]: "https://www.improved-initiative.com/e/"
+    });
   }
-})
+});
 
-runtime.onMessage.addListener(
-  function(request, sender, sendResponse) {
-    if (request.action === "perform-save") {
-      storage.get("target-url", async values => {
-        const compressed = await codec.compress(JSON.stringify(request.importedStatBlock));
-        tabs.create({
-          url: values["target-url"] + "?s=" + encodeURIComponent(compressed)
-        });
-        sendResponse({ action: "saved" });
+runtime.onMessage.addListener(function(request, sender, sendResponse) {
+  if (request.action === "perform-save") {
+    storage.get(Options.TargetUrl, async values => {
+      const compressed = await codec.compress(
+        JSON.stringify(request.importedStatBlock)
+      );
+      tabs.create({
+        url: values[Options.TargetUrl] + "?s=" + encodeURIComponent(compressed)
       });
-      
-    }
+      sendResponse({ action: "saved" });
+    });
   }
-);
+});
