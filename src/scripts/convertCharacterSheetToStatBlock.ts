17bf8cefd854c948e412fbcbc5e2bc2edf5c114e
Evan Bailey <baileyevang@gmail.com>
2020-04-22 22:03:28 -0700

    tolerate new ddbc- classname prefixes

--- src/scripts/convertCharacterSheetToStatBlock.ts
+++ src/scripts/convertCharacterSheetToStatBlock.ts
@@ -5,39 +5,39 @@ import { IsConditionImmunity } from "./IsConditionImmunity";
 
 export const convertCharacterSheetToStatBlock = (options: AllOptions) => {
   const doc = cash(document);
-  const characterSheetElement = doc.find(".ct-character-sheet");
+  const characterSheetElement = doc.find(prefix("character-sheet"));
   const statBlock: Partial<StatBlock> = {
     Source: "",
     Name: characterSheetElement
-      .find(".ct-character-tidbits__name")
+      .find(prefix("character-tidbits__name"))
       .text()
       .trim(),
     Type: characterSheetElement
-      .find(".ct-character-tidbits__race")
+      .find(prefix("character-tidbits__race"))
       .text()
       .trim(),
     HP: getHitPoints(characterSheetElement),
     AC: getArmorClass(characterSheetElement),
     Abilities: getAbilities(characterSheetElement),
-    Speed: [characterSheetElement.find(".ct-speed-box__box-value").text()],
+    Speed: [characterSheetElement.find(prefix("speed-box__box-value")).text()],
     // InitiativeModifier?: number,
     // InitiativeSpecialRoll?: "advantage" | "disadvantage" | "take-ten",
     // InitiativeAdvantage?: boolean,
     DamageVulnerabilities: getDefenses(characterSheetElement, "Vulnerability"),
     DamageResistances: getDefenses(characterSheetElement, "Resistance"),
     DamageImmunities: getDefenses(characterSheetElement, "Immunity").filter(
-      immunity => !IsConditionImmunity(immunity)
+      (immunity) => !IsConditionImmunity(immunity)
     ),
     ConditionImmunities: getDefenses(
       characterSheetElement,
       "Immunity"
-    ).filter(immunity => IsConditionImmunity(immunity)),
+    ).filter((immunity) => IsConditionImmunity(immunity)),
     Saves: getSaves(characterSheetElement),
     Skills: getSkills(characterSheetElement),
     Senses: getSenses(characterSheetElement),
     Languages: getLanguages(characterSheetElement),
     Challenge: characterSheetElement
-      .find(".ct-character-tidbits__classes")
+      .find(prefix("character-tidbits__classes"))
       .text()
       .trim(),
     Traits: [],
@@ -46,46 +46,44 @@ export const convertCharacterSheetToStatBlock = (options: AllOptions) => {
     LegendaryActions: [],
     ImageURL: getImageUrl(characterSheetElement),
     Description: "",
-    Player: "player"
+    Player: "player",
   };
   return statBlock;
 };
 
 function getHitPoints(element: Cash) {
-  const maxHP = element
-    .find(".ct-health-summary__hp-item-label")
+  let maxHP = element
+    .find(prefix("health-summary__hp-item-label"))
     .filter(
-      (_, label) =>
-        cash(label)
-          .text()
-          .trim()
-          .toLocaleLowerCase() == "max"
+      (_, label) => cash(label).text().trim().toLocaleLowerCase() == "max"
     )
-    .siblings(".ct-health-summary__hp-item-content")
+    .siblings(prefix("health-summary__hp-item-content"))
     .text()
     .trim();
 
+  if (!maxHP?.length) {
+    maxHP = element.find(".ct-status-summary-mobile__hp-max").text().trim();
+  }
+
   return {
     Value: parseInt(maxHP),
-    Notes: ""
+    Notes: "",
   };
 }
 
 function getArmorClass(element: Cash) {
   return {
     Value: parseInt(
-      element
-        .find(".ct-armor-class-box__value")
-        .text()
-        .trim()
+      element.find(prefix("armor-class-box__value")).text().trim()
     ),
-    Notes: ""
+    Notes: "",
   };
 }
 
 function getImageUrl(element: Cash) {
   const backgroundImageAttribute =
-    element.find(".ct-character-tidbits__avatar").css("background-image") || "";
+    element.find(prefix("character-tidbits__avatar")).css("background-image") ||
+    "";
   if (!backgroundImageAttribute.length) {
     return "";
   }
@@ -100,18 +98,20 @@ function getAbilities(element: Cash): AbilityScores {
     Con: getAbility(element, "con", abilityScoreSelector),
     Int: getAbility(element, "int", abilityScoreSelector),
     Wis: getAbility(element, "wis", abilityScoreSelector),
-    Cha: getAbility(element, "cha", abilityScoreSelector)
+    Cha: getAbility(element, "cha", abilityScoreSelector),
   };
 }
 
 function resolveAbilityScoreSelector(element: Cash) {
   const modifiersRegex = /[\+\-]/g;
   if (
-    modifiersRegex.test(element.find(".ct-ability-summary__secondary").text())
+    modifiersRegex.test(
+      element.find(prefix("ability-summary__secondary")).text()
+    )
   ) {
-    return ".ct-ability-summary__primary";
+    return prefix("ability-summary__primary");
   } else {
-    return ".ct-ability-summary__secondary";
+    return prefix("ability-summary__secondary");
   }
 }
 
@@ -122,11 +122,11 @@ function getAbility(
 ) {
   let score = 10;
   const scoreLabel = element
-    .find(".ct-ability-summary__abbr")
+    .find(prefix("ability-summary__abbr"))
     .filter((_, element: Element) => element.textContent == ability);
 
   const scoreText = scoreLabel
-    .parents(".ct-ability-summary")
+    .parents(prefix("ability-summary"))
     .find(abilityScoreSelector)
     .text();
   try {
@@ -138,20 +138,20 @@ function getAbility(
 function getDefenses(element: Cash, defenseType: string) {
   return element
     .find(`[data-original-title=${defenseType}]`)
-    .parents(".ct-defenses-summary__group")
-    .find(".ct-defenses-summary__defense")
+    .parents(prefix("defenses-summary__group"))
+    .find(prefix("defenses-summary__defense"))
     .get()
-    .map(el => cash(el).text());
+    .map((el) => cash(el).text());
 }
 
 function getSaves(element: Cash) {
   return element
     .find(`[data-original-title="Proficiency"]`)
-    .parents(".ct-saving-throws-summary__ability")
+    .parents(prefix("saving-throws-summary__ability"))
     .get()
-    .map(el => {
+    .map((el) => {
       const abilityName = cash(el)
-        .find(".ct-saving-throws-summary__ability-name")
+        .find(prefix("saving-throws-summary__ability-name"))
         .text();
       const titleCasedAbilityName =
         abilityName.substr(0, 1).toLocaleUpperCase() + abilityName.substr(1);
@@ -159,34 +159,31 @@ function getSaves(element: Cash) {
         Name: titleCasedAbilityName,
         Modifier: parseInt(
           cash(el)
-            .find(".ct-saving-throws-summary__ability-modifier")
+            .find(prefix("saving-throws-summary__ability-modifier"))
             .text()
-        )
+        ),
       };
     });
 }
 
 function getSkills(element: Cash) {
   return element
-    .find(`[data-original-title="Proficiency"], [data-original-title="Half Proficiency"], [data-original-title="Expertise"]`)
-    .parents(".ct-skills__item")
+    .find(
+      `[data-original-title="Proficiency"], [data-original-title="Half Proficiency"], [data-original-title="Expertise"]`
+    )
+    .parents(prefix("skills__item"))
     .get()
-    .map(el => {
+    .map((el) => {
       return {
-        Name: cash(el)
-          .find(".ct-skills__col--skill")
-          .text(),
-        Modifier: parseInt(
-          cash(el)
-            .find(".ct-signed-number")
-            .text()
-        )
+        Name: cash(el).find(prefix("skills__col--skill")).text(),
+        Modifier: parseInt(cash(el).find(prefix("signed-number")).text()),
       };
     });
 }
 
 function getSenses(element: Cash) {
-  const sensesString = element.find(".ct-senses__summary")
+  const sensesString = element
+    .find(prefix("senses__summary"))
     .text()
     .replace("Additional Sense Types", "");
   if (sensesString.length == 0) {
@@ -198,11 +195,15 @@ function getSenses(element: Cash) {
 
 function getLanguages(element: Cash) {
   return element
-    .find(".ct-proficiency-groups__group-label")
+    .find(prefix("proficiency-groups__group-label"))
     .filter((_, el) => cash(el).text() == "Languages")
-    .parents(".ct-proficiency-groups__group")
-    .find(".ct-proficiency-groups__group-items")
+    .parents(prefix("proficiency-groups__group"))
+    .find(prefix("proficiency-groups__group-items"))
     .text()
     .split(",")
-    .map(s => s.trim());
+    .map((s) => s.trim());
+}
+
+function prefix(suffix: string) {
+  return `.ct-${suffix}, .ddbc-${suffix}`;
 }
