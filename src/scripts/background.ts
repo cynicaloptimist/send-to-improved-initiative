5b52f422dbf7153657fce22377cdb5809f90dc57
twloveduck <tuckwilliamson@gmail.com>
2021-10-18 17:42:35 -0400

    Added link option, fixed chrome options display, #8 Adds an option to put a link in the description back to the source page. Addresses html doesn't display checkboxes in chrome/spruces up options. Addresses issue cynicaloptimist/send-to-improved-initiative#8 - reuse a single tab when importing.

--- src/scripts/background.ts
+++ src/scripts/background.ts
@@ -5,6 +5,7 @@ const codec = require("json-url")("lzma");
 
 const runtime: typeof chrome.runtime = ext.runtime;
 const tabs: typeof chrome.tabs = ext.tabs;
+var CurrentTabId = undefined;
 
 storage.get(Object.values(Options), values => {
   for (const option in OptionDefaults) {
@@ -16,15 +17,45 @@ storage.get(Object.values(Options), values => {
   }
 });
 
-runtime.onMessage.addListener(function(request, sender, sendResponse) {
+function newIITab(url: string) {
+  tabs.create({ url: url }, (tab) => {
+    CurrentTabId = tab.id;
+    console.log('New II tab id: ', CurrentTabId)
+  });
+}
+
+// Handles if the II tab is closed.
+tabs.onRemoved.addListener((tabId: number, remInfo: object) => {
+  if(CurrentTabId === tabId)
+    CurrentTabId = undefined
+})
+
+runtime.onMessage.addListener(function (request, sender, sendResponse) {
   if (request.action === "perform-save") {
     storage.get(Options.TargetUrl, async values => {
       const compressed = await codec.compress(
         JSON.stringify(request.importedStatBlock)
       );
-      tabs.create({
-        url: values[Options.TargetUrl] + "?s=" + encodeURIComponent(compressed)
-      });
+
+      let iiUrl = values[Options.TargetUrl] + "?s=" + encodeURIComponent(compressed)
+      if (CurrentTabId) {
+        try {
+          tabs.update(CurrentTabId, { url: iiUrl, active: true }, tab => {
+            if (!tab || tab.id === tabs.TAB_ID_NONE) {
+              console.error('Did not find tab')
+              newIITab(iiUrl)
+            }
+            else
+              console.log('Changed the existing tab url.')
+          })
+        }
+        catch (error) {
+          console.log('Err while getting tab.', error)
+        }
+      }
+      else
+        newIITab(iiUrl)
+
       sendResponse({ action: "saved" });
     });
   }
