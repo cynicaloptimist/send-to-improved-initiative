9b4dd11d138118b96d0ee895e66ebf4dad5c7f97
Evan Bailey <cynicaloptimist@gmail.com>
2025-02-09 18:58:27 -0800

    Ensure container takes full height

--- components/Container.tsx
+++ components/Container.tsx
@@ -1,6 +1,6 @@
 export function Container(props: { children: any; }) {
   return (
-    <div className="p-3 flex flex-col gap-2 text-base border-brand border-y-4">
+    <div className="p-3 flex flex-col gap-2 text-base border-brand border-y-4 h-full">
       {props.children}
     </div>
   );
